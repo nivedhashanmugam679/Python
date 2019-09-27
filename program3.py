af8322897a073836da6a7b2d5f85a2c7ec1bb061
nivedhashanmugam679 <35552026+nivedhashanmugam679@users.noreply.github.com>
2019-09-27 11:42:29 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,12 @@
+def stringfun():
+	print("Methods in string")
+	string1 = "Hello world"
+	print("Printing string1 ",string1)
+	print("Accessing characters in string ",string1[2:6])
+	string2 = string1.replace('world','everyone')
+	print("Replacing a word in the string ",string2)
+	print("Upper case of a string ",string2.upper())
+	print("Lower case of a string ",string1.lower())
+	print("Join in string ",string1.join("!!!!"))
+  
+stringfun()
